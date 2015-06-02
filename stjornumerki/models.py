fb967521fd9f3282b3223248e8b7df73ee60ada5
Martin Swift <martin@swift.is>
2015-06-02 05:36:35 +0000

    Bætti við módeli til að safna svörum.

--- stjornumerki/models.py
+++ stjornumerki/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class StjornumerkisSvar(models.Model):
+    STEINGEIT = 'ST'
+    VATNSBERI = 'VB'
+    FISKUR = 'FI'
+    HRUTUR = 'HR'
+    NAUT = 'NT'
+    TVIBURi = 'TV'
+    KRABBI = 'KR'
+    LJON = 'LJ'
+    MEYJA = 'MJ'
+    VOG = 'VG'
+    SPORDDREKI = 'SP'
+    BOGAMADUR = 'BO'
+    MERKI = (
+        ( STEINGEIT, 'steingeit' ),
+        ( VATNSBERI, 'vatnsberi' ),
+        ( FISKUR, 'fiskur' ),
+        ( HRUTUR, 'hrútur' ),
+        ( NAUT, 'naut' ),
+        ( TVIBURI, 'tvíburi' ),
+        ( KRABBI, 'krabbi' ),
+        ( LJON, 'ljón' ),
+        ( MEYJA, 'meyja' ),
+        ( VOG, 'vog' ),
+        ( SPORDDREKI, 'sporðdreki' ),
+        ( BOGAMADUR, 'bogamaður' ),
+    )
+    dags = models.DateTimeField()
+    faedingardags = models.DateTimeField()
+    merki = models.CharField(max_length=2, choices=MERKI)
+    valid = models.CharField(max_length=2, choices=MERKI)
+    utgafa = models.IntegerField(default=0)
