023664a652c0529d92765abbd2281384fa0b3c7e
Martin Swift <martin@swift.is>
2015-06-02 03:34:49 +0000

    Einfaldaði statísku síðurnar töluvert.

--- visindasmidjan/urls.py
+++ visindasmidjan/urls.py
@@ -1,25 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from visindasmidjan.views import StaticView
 
 urlpatterns = [
-    url(r'^$', 'visindasmidjan.views.forsida', name='forsida'),
+    #url(r'^$', 'visindasmidjan.views.forsida', name='forsida'),
+    url(r'^$', 'eldar.views.forsida', name='forsida'),
     url(r'^skraning/', include('skraning.urls')),
 
-    url(r'^itarefni/$', 'visindasmidjan.views.itarefni'),
-    url(r'^itarefni/forritun', 'visindasmidjan.views.forritun'),
-    url(r'^itarefni/hljodfaeri', 'visindasmidjan.views.hljodfaeri'),
-    url(r'^itarefni/hvirflar', 'visindasmidjan.views.hvirflar'),
-    url(r'^itarefni/jardfraedi', 'visindasmidjan.views.jardfraedi'),
-    url(r'^itarefni/rafmagnsfraedi', 'visindasmidjan.views.rafmagnsfraedi'),
-    url(r'^itarefni/ljos_og_litir', 'visindasmidjan.views.ljos_og_litir'),
-    url(r'^itarefni/girar_og_trissur', 'visindasmidjan.views.girar_og_trissur'),
-    url(r'^itarefni/thrautir', 'visindasmidjan.views.thrautir'),
-    url(r'^itarefni/rolur_og_adrir_pendular', 'visindasmidjan.views.rolur_og_adrir_pendular'),
-    url(r'^itarefni/speglar', 'visindasmidjan.views.speglar'),
-    url(r'^itarefni/stjornufraedi', 'visindasmidjan.views.stjornufraedi'),
-    url(r'^itarefni/visindaheimspeki', 'visindasmidjan.views.visindaheimspeki'),
+    url(r'^itarefni/$', TemplateView.as_view(template_name='itarefni/index.html'), { 'page': 'itarefni', } ),
+    url(r'^itarefni/(?P<page>.+)$', StaticView.as_view()),
 
     url(r'^um_visindasmidjuna/', 'visindasmidjan.views.um_visindasmidjuna'),
+
+    url(r'^stjornumerki/', include('stjornumerki.urls')),
 
     url(r'^konnun/', include('konnun.urls')),
 
--- visindasmidjan/views.py
+++ visindasmidjan/views.py
@@ -1,41 +1,55 @@
 # -*- encoding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.views.generic import TemplateView
 
 def forsida(request):
     return render_to_response('forsida.html', { 'page': 'forsida', })
 
-def itarefni(request):
-    return render_to_response('itarefni/index.html', { 'page': 'itarefni', })
+#def itarefni(request):
+#    return render_to_response('itarefni/index.html', { 'page': 'itarefni', })
 
 # Ítarefnissíður
-def speglar(request):
-    return render_to_response('itarefni/speglar.html', { 'page': 'itarefni', })
-def hvirflar(request):
-    return render_to_response('itarefni/hvirflar.html', { 'page': 'itarefni', })
-def rolur_og_adrir_pendular(request):
-    return render_to_response('itarefni/rolur_og_adrir_pendular.html', { 'page': 'itarefni', })
-def hljodfaeri(request):
-    return render_to_response('itarefni/hljodfaeri.html', { 'page': 'itarefni', })
+# frá http://stackoverflow.com/questions/3402708/django-urls-straight-to-html-template
+from django.template import TemplateDoesNotExist
+from django.http import Http404
 
-def rafmagnsfraedi(request):
-    return render_to_response('itarefni/rafmagnsfraedi.html', { 'page': 'itarefni', })
-def ljos_og_litir(request):
-    return render_to_response('itarefni/ljos_og_litir.html', { 'page': 'itarefni', })
-def girar_og_trissur(request):
-    return render_to_response('itarefni/girar_og_trissur.html', { 'page': 'itarefni', })
-def thrautir(request):
-    return render_to_response('itarefni/thrautir.html', { 'page': 'itarefni', })
-
-# Vísindaspjall og verkefni
-def visindaheimspeki(request):
-    return render_to_response('itarefni/visindaheimspeki.html', { 'page': 'itarefni', })
-def jardfraedi(request):
-    return render_to_response('itarefni/jardfraedi.html', { 'page': 'itarefni', })
-def stjornufraedi(request):
-    return render_to_response('itarefni/stjornufraedi.html', { 'page': 'itarefni', })
-def forritun(request):
-    return render_to_response('itarefni/forritun.html', { 'page': 'itarefni', })
+class StaticView(TemplateView):
+    def get(self, request, page, *args, **kwargs):
+        self.template_name = 'itarefni/'+page+'.html'
+        response = super(StaticView, self).get(request, *args, **kwargs)
+        try:
+            return response.render()
+        except TemplateDoesNotExist:
+            raise Http404()
+#
+#def speglar(request):
+#    return render_to_response('itarefni/speglar.html', { 'page': 'itarefni', })
+#def hvirflar(request):
+#    return render_to_response('itarefni/hvirflar.html', { 'page': 'itarefni', })
+#def rolur_og_adrir_pendular(request):
+#    return render_to_response('itarefni/rolur_og_adrir_pendular.html', { 'page': 'itarefni', })
+#def hljodfaeri(request):
+#    return render_to_response('itarefni/hljodfaeri.html', { 'page': 'itarefni', })
+#
+#def rafmagnsfraedi(request):
+#    return render_to_response('itarefni/rafmagnsfraedi.html', { 'page': 'itarefni', })
+#def ljos_og_litir(request):
+#    return render_to_response('itarefni/ljos_og_litir.html', { 'page': 'itarefni', })
+#def girar_og_trissur(request):
+#    return render_to_response('itarefni/girar_og_trissur.html', { 'page': 'itarefni', })
+#def thrautir(request):
+#    return render_to_response('itarefni/thrautir.html', { 'page': 'itarefni', })
+#
+## Vísindaspjall og verkefni
+#def visindaheimspeki(request):
+#    return render_to_response('itarefni/visindaheimspeki.html', { 'page': 'itarefni', })
+#def jardfraedi(request):
+#    return render_to_response('itarefni/jardfraedi.html', { 'page': 'itarefni', })
+#def stjornufraedi(request):
+#    return render_to_response('itarefni/stjornufraedi.html', { 'page': 'itarefni', })
+#def forritun(request):
+#    return render_to_response('itarefni/forritun.html', { 'page': 'itarefni', })
 
 #def fasar_efnis(request):
 #    return render_to_response('itarefni/fasar_efnis.html', { 'page': 'itarefni', })
